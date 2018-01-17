795842bd2064e29b3dbe168ae81ca92198929f58
Naraphat <naraphat.nam@gmail.com>
2018-01-17 00:09:54 -0800

    ''

---
+++ src/test/java/rmuti/stack/TestStack.java
@@ -0,0 +1,20 @@
+package rmuti.stack;
+
+import rmuti.stack.ArrayListStack;
+
+public class TestStack{
+		
+		public static void main(String[] args) {
+			ArrayListStack stack = new ArrayListStack();
+			stack.push("macos");
+			stack.push("linux");
+			stack.push("windows");
+			System.out.println(stack);
+			
+			String data = (String)stack.pop();
+			System.out.println(stack);
+			
+			
+		}
+	}
+
